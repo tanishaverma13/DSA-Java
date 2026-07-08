ecbb899e7c6789f776c58e28b4babd10763252bf
Tanisha Verma <tanishaverma219@gmail.com>
2026-07-08 11:53:54 +0530

    Initial commit: Added Arrays & Hashing solutions

---
+++ 01-Arrays-Hashing/ValidAnagram.java
@@ -0,0 +1,48 @@
+/*
+-------------------------------------------------------
+LeetCode 242 - Valid Anagram
+
+Pattern:
+Frequency Counting
+
+Difficulty:
+Easy
+
+Approach:
+Frequency Array
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(1)
+
+Key Learning:
+Compare character frequencies instead of character order.
+-------------------------------------------------------
+*/
+
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+
+            if (count != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
